61811e9c1316cc27e6ec9455d63a19245a0f3a33
Mickey Rose <lightmare@users.noreply.github.com>
2021-06-17 14:01:13 +0200

    add broken tests for unparenthesized arrow functions in F# pipeline

--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/fsharp/arrow-functions-parenless/exec.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/fsharp/arrow-functions-parenless/exec.js
@@ -25,3 +25,15 @@ expect( _f(1)).toBe(4);
 expect(  g(1)).toBe(2);
 expect( _g(1)).toBe(2);
 expect(__g(1)).toBe(2);
+
+const cache = {};
+const work = x => x ** 2;
+
+const memoizeF =           y => cache[y] ??= y |> work;
+const memoizeG = x => x |> y => cache[y] ??= y |> work;
+
+expect(memoizeF(2)).toBe(4);
+expect(cache[2]).toBe(4);
+
+expect(memoizeG(3)).toBe(9);
+expect(cache[3]).toBe(9);
