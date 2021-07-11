865e6838e5f8afffa124d0c836c3e62799c4c3a5
Mickey Rose <lightmare@users.noreply.github.com>
2021-07-11 18:07:53 +0200

    avoid recursion in generator expression's tail spread

--- packages/babel-plugin-proposal-generator-literals/src/index.js
+++ packages/babel-plugin-proposal-generator-literals/src/index.js
@@ -3,7 +3,53 @@ import syntaxGeneratorLiterals from "@babel/plugin-syntax-generator-literals";
 import { types as t } from "@babel/core";
 import template from "@babel/template";
 
-const literal = template.expression("{[Symbol.iterator]: function*()%%body%%}");
+const generatorEmpty = template.expression(`
+{
+  [Symbol.iterator]: function* () {}
+}
+`);
+
+const generatorNoSpread = template.expression(`
+(kList =>
+  ({
+    [kList]: %%LIST%%,
+    [Symbol.iterator]: function* () {
+      let result;
+      for (const getter of this[kList]) {
+        result = yield getter();
+      }
+      return result;
+    }
+  })
+)(Symbol.for('##generatorExpressionList'))
+`);
+
+const generatorWithTail = template.expression(`
+((kHead, kTail) =>
+  ({
+    [kHead]: %%HEAD_ARRAY%%,
+    [kTail]: %%TAIL_FUNC%%,
+    [Symbol.iterator]: function* () {
+      let iter = this;
+      do {
+        for (const getter of iter[kHead]) {
+          yield* getter();
+        }
+        iter = iter[kTail]();
+      } while (kHead in iter);
+      return yield* iter;
+    }
+  })
+)(Symbol.for('##generatorExpressionHead'), Symbol.for('##generatorExpressionTail'))
+`);
+
+function makeYieldGetter(expr) {
+  return t.arrowFunctionExpression([], expr);
+}
+
+function makeYieldIterable(expr) {
+  return t.isSpreadElement(expr) ? expr.argument : t.arrayExpression([expr]);
+}
 
 export default declare(api => {
   api.assertVersion(7);
@@ -13,18 +59,24 @@ export default declare(api => {
     inherits: syntaxGeneratorLiterals,
     visitor: {
       GeneratorExpression(path) {
-        path.replaceWith(
-          literal({
-            body: t.blockStatement(
-              path.node.elements.map(node => {
-                const isSpread = t.isSpreadElement(node);
-                return t.expressionStatement(
-                  t.yieldExpression(isSpread ? node.argument : node, isSpread),
-                );
-              }),
-            ),
-          }),
-        );
+        const { elements } = path.node;
+        if (elements.length === 0) {
+          path.replaceWith(generatorEmpty());
+        } else if (elements.some(expr => t.isSpreadElement(expr))) {
+          const getters = elements.map(makeYieldIterable).map(makeYieldGetter);
+          const tail = getters.pop();
+          path.replaceWith(
+            generatorWithTail({
+              HEAD_ARRAY: t.arrayExpression(getters),
+              TAIL_FUNC: tail,
+            }),
+          );
+        } else {
+          const getters = elements.map(makeYieldGetter);
+          path.replaceWith(
+            generatorNoSpread({ LIST: t.arrayExpression(getters) }),
+          );
+        }
       },
     },
   };
